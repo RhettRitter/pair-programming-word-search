ecaac12bcc2b265384aec42c18b8f34e203f4789
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-13 23:02:25 +0000

    word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,23 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['B', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['U', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['T', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['R', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['A', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BUSTER')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontially", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +50,11 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return false if the letters array is empty", function() {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
 const wordSearch = (letters, word) => { 
+    if(letters.length === 0) {
+        return false;
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
+    //console.log(horizontalJoin);
+
+    const transposedLetters = letters[0].map((_, colIndex) => letters.map(row => row[colIndex]));
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+    //console.log(verticalJoin);
+
+    
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+
+    return false;
+};
 
 module.exports = wordSearch
+
+
+/**
+ * 
+ * use search to look up words
+ * 
+ * create an array of vertical words
+ * create an array of horizontal words
+ * Match words
+ * 
+ * 
+ * Matrix
+ * searching Horizontally
+ * searching vertically
+ * 
+ * 
+ * 
+ */
